82b371a5d87cd83bfd0795f3a77c99e48f423af9
Dostonlv <iamitachiuchixa@gmail.com>
2023-02-05 14:41:05 +0500

    [feature]: routes - complete🎉

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+	"restaurant-management/database"
+	"restaurant-management/routes"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("Port")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "restaurant-management/controllers"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/user/:user_id", controller.GetUser())
+	incomingRoutes.POST("/users/signup", controller.SignUp())
+	incomingRoutes.POST("/users/login", controller.Login())
+
+}
